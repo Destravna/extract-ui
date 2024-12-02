768857fe6a4a170b8aae00afb5cac44a136cfe91
Dhruv Singh <kingrajput450@gmail.com>
2024-12-02 14:30:56 +0530

    dropdowns, job status page

--- src/components/AddExtract.jsx
+++ src/components/AddExtract.jsx
@@ -1,64 +1,78 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 import { Dropdown } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const AddExtract = () => {
   const [formData, setFormData] = useState({
-    extractName: '',
-    model: '',
+    extractName: "",
+    model: "",
     attributes: {
       allAttributes: false,
-      attributesList: {} 
+      attributesList: [], // Change attributesList to an array
     },
-    dateFrom: '',
-    dateTo: '',
-    outputFormat: '',
-    outputPath: '',
-    deliveryFormat: '',
+    dateFrom: "",
+    dateTo: "",
+    outputFormat: "",
+    outputPath: "",
+    deliveryFormat: "",
   });
 
   const [newAttribute, setNewAttribute] = useState({
-    attributeName: '',
-    alias: '',
-    condition: ''
+    attributeName: "",
+    alias: "",
+    condition: "",
   });
 
   const attributeOptions = [
-    "TradeDirection", "tradeType", "IsDestroyed", "redid", "comment", "tradeDate", 
-    "tradeValueDate", "lakeStorageTimeStamp", "modelVersion", "nominalAmount", 
-    "nominalCurrency", "quantity", "rawStorageTimeStamp", "sourceApplication", 
-    "tradeld", "version", "lastUpdateTimeStamp", "product", "product.productid", 
-    "Product.productName", "Product.maturity"
+    "TradeDirection",
+    "tradeType",
+    "IsDestroyed",
+    "redid",
+    "comment",
+    "tradeDate",
+    "tradeValueDate",
+    "lakeStorageTimeStamp",
+    "modelVersion",
+    "nominalAmount",
+    "nominalCurrency",
+    "quantity",
+    "rawStorageTimeStamp",
+    "sourceApplication",
+    "tradeld",
+    "version",
+    "lastUpdateTimeStamp",
+    "product",
+    "product.productid",
+    "Product.productName",
+    "Product.maturity",
   ];
 
-  // Handle input change for new attribute fields
   const handleNewAttributeChange = (event) => {
     const { name, value } = event.target;
     setNewAttribute({ ...newAttribute, [name]: value });
   };
 
   const handleAddAttribute = () => {
-    setFormData(prevState => {
-      return {
-        ...prevState,
-        attributes: {
-          ...prevState.attributes,
-          attributesList: {
-            ...prevState.attributes.attributesList,
-            [newAttribute.attributeName]: {
-              alias: newAttribute.alias,
-              condition: newAttribute.condition
-            }
-          }
-        }
-      };
-    });
-  
-    // Clear the new attribute inputs after adding
-    setNewAttribute({ attributeName: '', alias: '', condition: '' });
-    console.log("attribute added");
-    console.log(formData);  // Will still show the previous state, but now the new state will be reflected in the next render
+    const newAttributeObject = {
+      [newAttribute.attributeName]: {
+        alias: newAttribute.alias,
+        condition: newAttribute.condition,
+      },
+    };
+
+    setFormData((prevState) => ({
+      ...prevState,
+      attributes: {
+        ...prevState.attributes,
+        attributesList: [...prevState.attributes.attributesList, newAttributeObject],
+      },
+    }));
+
+    setNewAttribute({ attributeName: "", alias: "", condition: "" });
+    console.log("attribute added", formData);
   };
 
   const handleInputChange = (event) => {
@@ -66,64 +80,129 @@ const AddExtract = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  // Handle dropdown selection change for output format
   const handleOutputFormatChange = (eventKey) => {
     setFormData({ ...formData, outputFormat: eventKey });
-    console.log(formData);
   };
 
   const handleAllAttributesChange = (eventKey) => {
     setFormData({
-      ...formData, 
+      ...formData,
       attributes: {
-        ...formData.attributes, 
-        allAttributes: eventKey
-      }
+        ...formData.attributes,
+        allAttributes: eventKey,
+      },
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('submit');
+    console.log("submit");
     console.log(formData);
     const jsonString = JSON.stringify(formData);
     console.log("json string");
     console.log(jsonString);
+
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "http://localhost:4000/api/add-extract",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: jsonString,
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}  style={{ padding: '10px' }}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="text" name="extractName" value={formData.extractName} onChange={handleInputChange} placeholder="extract-name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control type="text" name="model" value={formData.model} onChange={handleInputChange} placeholder="otc or listed" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control type="date" name="dateFrom" value={formData.dateFrom} onChange={handleInputChange} placeholder="Date from" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control type="date" name="dateTo" value={formData.dateTo} onChange={handleInputChange} placeholder="Date to" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control type="text" name="outputPath" value={formData.outputPath} onChange={handleInputChange} placeholder="outputPath" />
-      </Form.Group>
+    <Form  onSubmit={handleSubmit} style={{ padding: "10px" }}>
+      <p>
+        Extract name
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="extractName"
+            value={formData.extractName}
+            onChange={handleInputChange}
+            placeholder="extract-name"
+          />
+        </Form.Group>
+      </p>
 
-      <Dropdown onSelect={handleOutputFormatChange}>
-        <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
-          {formData.outputFormat || "Output Format"}
-        </Dropdown.Toggle>
+      <p>
+        Model
+        <Dropdown
+          onSelect={(eventKey) =>
+            setFormData({ ...formData, model: eventKey })
+          }
+        >
+          <Dropdown.Toggle variant="Secondary" id="dropdown-model">
+            {formData.model || "Select Model"}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="otc">OTC</Dropdown.Item>
+            <Dropdown.Item eventKey="listed">Listed</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </p>
 
-        <Dropdown.Menu>
-          <Dropdown.Item eventKey="avro">avro</Dropdown.Item>
-          <Dropdown.Item eventKey="csv">csv</Dropdown.Item>
-          <Dropdown.Item eventKey="xml">xml</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+      <p>
+        Date From
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="date"
+            name="dateFrom"
+            value={formData.dateFrom}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+      </p>
+
+      <p>
+        Date To
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="date"
+            name="dateTo"
+            value={formData.dateTo}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+      </p>
+
+      <p>
+        Output Path
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            name="outputPath"
+            value={formData.outputPath}
+            onChange={handleInputChange}
+            placeholder="outputPath"
+          />
+        </Form.Group>
+      </p>
+
+      <p>
+        Output Format
+        <Dropdown onSelect={handleOutputFormatChange}>
+          <Dropdown.Toggle variant="Secondary">
+            {formData.outputFormat || "Output Format"}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="avro">avro</Dropdown.Item>
+            <Dropdown.Item eventKey="csv">csv</Dropdown.Item>
+            <Dropdown.Item eventKey="xml">xml</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </p>
 
       <Dropdown onSelect={handleAllAttributesChange}>
-        <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
-          {formData.attributes.allAttributes || "get All attributes"}
+        <Dropdown.Toggle variant="Secondary">
+          {formData.attributes.allAttributes || "Get All Attributes"}
         </Dropdown.Toggle>
         <Dropdown.Menu>
           <Dropdown.Item eventKey="true">true</Dropdown.Item>
@@ -131,46 +210,51 @@ const AddExtract = () => {
         </Dropdown.Menu>
       </Dropdown>
 
-      {
-        formData.attributes.allAttributes === 'false' && (
-          <div>
-            <Dropdown onSelect={(eventKey) => setNewAttribute({ ...newAttribute, attributeName: eventKey })}>
-              <Dropdown.Toggle variant="Secondary" id="dropdown-attribute-name">
-                {newAttribute.attributeName || "Select Attribute Name"}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {attributeOptions.map((option, index) => (
-                  <Dropdown.Item key={index} eventKey={option}>{option}</Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
+      {formData.attributes.allAttributes === "false" && (
+        <div>
+          {formData.attributes.attributesList.map((attribute, index) => (
+            <p key={index}>
+              <strong>Attribute:</strong> {Object.keys(attribute)[0]}, 
+              <strong> Alias:</strong> {Object.values(attribute)[0].alias || "N/A"}
+            </p>
+          ))}
 
-            <Form.Group className="mb-3" controlId="attributeAlias">
-              <Form.Control
-                type="text"
-                name="alias"
-                value={newAttribute.alias}
-                onChange={handleNewAttributeChange}
-                placeholder="Attribute Alias"
-              />
-            </Form.Group>
+          <Dropdown
+            onSelect={(eventKey) =>
+              setNewAttribute({ ...newAttribute, attributeName: eventKey })
+            }
+          >
+            <Dropdown.Toggle variant="Secondary">
+              {newAttribute.attributeName || "Select Attribute Name"}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {attributeOptions.map((option, index) => (
+                <Dropdown.Item key={index} eventKey={option}>
+                  {option}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
 
-            <Form.Group className="mb-3" controlId="attributeCondition">
-              <Form.Control
-                type="text"
-                name="condition"
-                value={newAttribute.condition}
-                onChange={handleNewAttributeChange}
-                placeholder="Attribute Condition"
-              />
-            </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              name="alias"
+              value={newAttribute.alias}
+              onChange={handleNewAttributeChange}
+              placeholder="Attribute Alias"
+            />
+          </Form.Group>
 
-            <Button onClick={handleAddAttribute} variant="dark">Add Attribute</Button>
-          </div>
-        )
-      }
+          <Button onClick={handleAddAttribute} variant="dark">
+            Add Attribute
+          </Button>
+        </div>
+      )}
 
-      <Button className="mt-3" type="submit" variant="warning">Submit</Button>
+      <Button className="mt-3" type="submit" variant="warning">
+        Submit
+      </Button>
     </Form>
   );
 };
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,6 +1,52 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
+
+
 const Jobs = () => {
+
+  const [data, setData] = useState([]);
+
+  const getJobs = async() => {
+    const data = await axios({
+      method : "GET",
+      url : "http://localhost:4000/api/get-all-jobs"
+    });
+    console.log(data.data);
+    setData(data.data)
+  }
+
+  useEffect(()=>{
+    getJobs();
+  }, [])
+
   return (
-    <div>Jobs</div>
+    <div  className="container mt-5">
+      <h2>Extracts Table</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Job ID</th>
+            <th>Extract ID</th>
+            <th>Extract Name</th>
+            <th>Extract Status</th>
+            <th>file link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.jobId}>
+              <td>{item.jobId}</td>
+              <td>{item.extractId}</td>
+              <td>{item.extractName}</td>
+              <td>{item.extractStatus}</td>
+              <td><a href={item.file} target="blank">{item.file}</a></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
